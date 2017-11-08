819421050c10132e02ae38a663c6fa5fcdd22ab8
Jonathan Zhang <jz2814@columbia.edu>
2017-11-08 01:30:30 -0500

    more c library functions

--- microc-upgrade/lib/src/eigen_test.h
+++ microc-upgrade/lib/src/eigen_test.h
@@ -5,9 +5,25 @@ extern "C" {
 #endif
 
 typedef void * matrix_t;
-matrix_t matrix_init(char, int, int);
-matrix_t mmadd(matrix_t m1, matrix_t m2);
-matrix_t fmadd(double s, matrix_t m);
+
+void print_mat(matrix_t);
+
+matrix_t init_fmat_zero(int, int);
+matrix_t init_fmat_const(double, int, int);
+matrix_t init_fmat_identity(int, int);
+
+void del_mat(matrix_t);
+
+matrix_t mmadd(matrix_t, matrix_t);
+matrix_t mmsub(matrix_t, matrix_t);
+matrix_t dot(matrix_t, matrix_t);
+
+matrix_t fmadd(double, matrix_t);
+matrix_t fmsub(double, matrix_t);
+matrix_t fmprod(double, matrix_t);
+matrix_t fmdiv(double, matrix_t);
+
+matrix_t mmcoefprod(matrix_t, matrix_t);
 
 #ifdef __cplusplus
 }
--- microc-upgrade/lib/src/eigen_test_lib.cpp
+++ microc-upgrade/lib/src/eigen_test_lib.cpp
@@ -5,16 +5,60 @@ Eigen::MatrixXd* mat_cast(matrix_t undef_mptr){
 	return static_cast<Eigen::MatrixXd*>(undef_mptr);
 }
 
-matrix_t matrix_init(char t, int d1, int d2){
-	Eigen::MatrixXd * tmp = new Eigen::MatrixXd(d1, d2);
-	(*tmp).setZero();
-	std::cout << *tmp << std::endl;
-	return tmp;
+void print_mat(matrix_t undef_mptr){
+	Eigen::MatrixXd* def_mptr = mat_cast(undef_mptr);
+	std::cout << *def_mptr << std::endl;
+}
+
+matrix_t init_fmat_zero(const int d1, const int d2){
+	Eigen::MatrixXd * tmp_mptr = new Eigen::MatrixXd;
+	*tmp_mptr = Eigen::MatrixXd::Zero(d1, d2);
+	// std::cout << *tmp_mptr << std::endl;
+	return tmp_mptr;
+}
+
+matrix_t init_fmat_const(const double c, const int d1, const int d2){
+	Eigen::MatrixXd* tmp_mptr = new Eigen::MatrixXd;
+	*tmp_mptr = Eigen::MatrixXd::Constant(d1, d2, c);
+	
+ 	return tmp_mptr;
+}
+
+matrix_t init_fmat_identity(const int d1, const int d2){
+	Eigen::MatrixXd* tmp_mptr = new Eigen::MatrixXd;
+	*tmp_mptr = Eigen::MatrixXd::Identity(d1, d2);
+
+	return tmp_mptr;
+}
+
+void del_mat(matrix_t undef_mptr){
+	Eigen::MatrixXd * def_ptr = mat_cast(undef_mptr);
+	delete def_ptr;
 }
 
 matrix_t mmadd(matrix_t undef_mptr1, matrix_t undef_mptr2){
 	Eigen::MatrixXd* def_mptr1 = mat_cast(undef_mptr1);
 	Eigen::MatrixXd* def_mptr2 = mat_cast(undef_mptr2);
-	*def_mptr1 += *def_mptr2;
-	return def_mptr1;
+	Eigen::MatrixXd* tmp_mptr = new Eigen::MatrixXd;
+
+	*tmp_mptr = *def_mptr1 + *def_mptr2;
+	return tmp_mptr;
+}
+
+matrix_t msub(matrix_t undef_mptr1, matrix_t undef_mptr2){
+	Eigen::MatrixXd* def_mptr1 = mat_cast(undef_mptr1);
+	Eigen::MatrixXd* def_mptr2 = mat_cast(undef_mptr2);
+	Eigen::MatrixXd* tmp_mptr = new Eigen::MatrixXd;
+
+	*tmp_mptr = *def_mptr1 - *def_mptr2;
+	return tmp_mptr;
+}
+
+matrix_t dot(matrix_t undef_mptr1, matrix_t undef_mptr2){
+	Eigen::MatrixXd* def_mptr1 = mat_cast(undef_mptr1);
+	Eigen::MatrixXd* def_mptr2 = mat_cast(undef_mptr2);
+	Eigen::MatrixXd* tmp_mptr = new Eigen::MatrixXd;
+
+	*tmp_mptr = *def_mptr1 * *def_mptr2;
+	return tmp_mptr;
 }
